86d61a930f12a879b26c1e5130b86b76dd9c14f5
Krishanu Mishra <77398741+KrishanuMishra@users.noreply.github.com>
2023-09-25 15:57:19 +0530

    DBMS Continued..

--- app/posts/[slug]/page.tsx
+++ app/posts/[slug]/page.tsx
@@ -27,7 +27,6 @@ const PostPage = (props: any) => {
         <h1 className="text-2xl text-slate-600 ">{post.data.title}</h1>
         <p className="text-slate-400 mt-2">{post.data.date}</p>
       </div>
-
       <article className="prose">
         <Markdown>{post.content}</Markdown>
       </article>
